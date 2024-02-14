04c333eced18e74b8ce0788a83a80c9cf1efe433
Abdullah Al Akash <aabdullahalakash@gmail.com>
2024-02-15 02:50:55 +0600

    Apply Section Complete

--- src/Pages/HomePage/HomePage.jsx
+++ src/Pages/HomePage/HomePage.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Banner from "./Banner";
+import ApplySection from "./ApplySection";
 
 const HomePage = () => {
   return (
     <div>
       <Banner></Banner>
+      <ApplySection></ApplySection>
     </div>
   );
 };
